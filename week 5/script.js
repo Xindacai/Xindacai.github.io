b4938d62c8a9d8a9d7126de5f667de6ecd824278
Xindacai <162404529+Xindacai@users.noreply.github.com>
2025-04-01 10:20:28 +1100

    d

--- week 5/script.js
+++ week 5/script.js
@@ -1,7 +1,13 @@
 console.log("hi");
 console.log("how are you buddy?")
+// this is a comment
+
+// I am declaring a variable called myName to contain my name
 let myName = "rohit";
 console.log("Hi, I am", myName);
 let my ID = "s12345";
 console.log("Hi, my student id is", myID);
-
+myName = "abcd";
+console.log("Hi, I am", myName);
+let myNewName = prompt("What is your name");
+console.log("Hello", myNewName);
